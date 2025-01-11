78129874a0f1d9888c163160c061f09f526af0a0
Tony Dugué <duguetony@gmail.com>
2025-01-11 17:53:14 +0100

    feat(layout): fix style footer

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { WhiteRabbitLogoComponent } from "../../shared/icon-svg/white-rabbit-logo.component";
 
 @Component({
   selector: 'app-footer',
-  imports: [FontAwesomeModule, WhiteRabbitLogoComponent],
+  imports: [],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
